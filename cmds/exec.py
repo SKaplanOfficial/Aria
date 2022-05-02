99751b78641b1589dacdb2c5b93690ede103754c
Stephen Kaplan <stephen.kaplan@maine.edu>
2022-05-02 14:28:19 -0400

    Improve code comments, make better use of tracking tools library, improve overall reliability of exec and jump commands

---
+++ cmds/exec.py
@@ -0,0 +1,181 @@
+"""
+x
+
+Last Updated: Version 0.0.1
+"""
+
+from datetime import datetime
+from time import sleep
+
+from CommandTypes import Command
+from AriaUtils import Blah
+
+class Execute(Command):
+    info = {
+        "title": "Execute",
+        "repository": "https://github.com/SKaplanOfficial/Aria",
+        "documentation_link": "https://github.com/SKaplanOfficial/Aria/documentation/",
+        'id': "aria_exec",
+        "version": "1.0.0",
+        "description": """
+            Executes a set of commands.
+            Tracks executions to better infer targets from shorthand notations over time.
+        """,
+        "requirements": {},
+        "extensions": {
+            'aria_exec': '1.0.0',
+        },
+        "purposes": [
+            "execute tasks", "run commands",
+        ],
+        "targets": [
+            "j google", "app calendar", "note today",
+        ],
+        "keywords": [
+            "aria", "command", "navigation", "shortcut",
+        ],
+        "example_usage": [
+            ("x example", "Runs the execution sequences named 'example'."),
+            ("x app calendar & app contacts --name events", "Creates a new execution sequence named 'events'."),
+        ],
+        "help": [
+            "This command executes a set of commands.",
+            "Run that",
+        ],
+        "contact_details": {
+            "author": "Stephen Kaplan",
+            "email": "stephen.kaplan@maine.edu",
+            "website": "http://skaplan.io",
+        }
+    }
+
+    def __init__(self, ):
+        self.force_context = False
+
+    def execute(self, query, origin):
+        blah2 = Blah("jello")
+        blah2.get_wow()
+        return
+        
+        # Extract targets from query
+        query_type = self.get_query_type(query)
+        target_destinations = query.split()
+        if query_type != 1:
+            target_destinations = target_destinations[1:]
+
+        # Initialize execution tracker object
+        item_structure = {
+            "name" : str,
+            "time" : float,
+            "frequency" : int,
+            "targets" : list,
+        }
+
+        exec_tracker = Command.managers["tracking"].tracker(
+            "exec",
+            item_structure = item_structure,
+            data_source = self.parse_target,
+            merge_method = self._increment_freq
+        )
+        exec_tracker.load_data()
+
+        # Get list of relevant candidates
+        candidates = exec_tracker.get_items_containing("targets", target_destinations)
+
+        # Create an object for the target jump(s)
+        data = self.parse_target(query)
+        exec_target = exec_tracker.new_item(data)
+        candidates += exec_tracker.get_items_containing("name", data[0])
+
+        # Find the best candidate, if one exists
+        best_candidate = self._get_best_candidate(exec_target, candidates, exec_tracker)
+        if best_candidate == None:
+            best_candidate = exec_target
+            print("New exec pathway:", best_candidate.data["name"], "->", best_candidate.data["targets"])
+            exec_tracker.add_item(best_candidate)
+        else:
+            print("Existing exec pathway:", best_candidate.data["name"], "->", best_candidate.data["targets"])
+        
+        exec_tracker.save_data()
+
+        if self.force_context:
+            Command.managers["context"].blank_context()
+
+        commands = best_candidate.data["targets"].split("|")
+        for command in commands:
+            print("Running", command)
+            Command.managers["input"].pseudo_input(command)
+            sleep(0.1)
+
+    def _get_best_candidate(self, exec_target, candidates, exec_tracker):
+        """ Gets the best exec candidate with matching or near-matching targets, if one exists. Returns None otherwise. """
+        max_freq = exec_tracker.get_max_of_col("frequency")
+
+        def compare_method(item_1, item_2):
+            if item_1.data["name"] == item_2.data["name"]:
+                return 0
+
+            score = exec_tracker.default_compare_method(item_1, item_2)
+            for target_1 in item_1.data["targets"]:
+                for target_2 in item_2.data["targets"]:
+                    if target_1.lower() in target_2.lower():
+                        score /= 1.1
+                    else:
+                        score *= 1.01
+
+            score -= (item_2.data["frequency"]) / max_freq
+            return score
+
+        best_candidate = exec_tracker.get_best_match(exec_target, candidates, 0.1, compare_method = compare_method)
+        return best_candidate
+
+    def _increment_freq(self, item_1, item_2):
+        """ Updates the exec item's frequency. """
+        item_1.data["frequency"] += 1
+        return item_1
+
+    def parse_target(self, target):
+        targets = target.split()[1:]
+
+        self.force_context = False
+        if "-fc" in targets:
+            self.force_context = True
+            targets.remove("-fc")
+
+        # Name
+        name = targets[0]
+        if "--name" in targets:
+            name_index = targets.index("--name") + 1
+            name = targets[name_index]
+            targets.pop(name_index)
+            targets.remove("--name")
+
+        # Time
+        now = datetime.now()
+        current_time = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+
+        targets_str = (" ".join(targets)).split(" & ")
+        return [name, current_time, 1, targets_str]
+
+    def get_query_type(self, query):
+        parts = query.split()
+        if parts[0] in ["x", "exec", "run"]:
+            return 1000
+        return 0
+
+    def get_template(self, new_cmd_name):
+        print("Enter base command and args: ")
+        cmd_new = input()
+
+        query_length = len(new_cmd_name)+1
+
+        template = {
+            'command': str(cmd_new.split(" ")),
+            'targets': 'query['+str(query_length)+':]',
+            'data_file_name': "'"+str(cmd_new.split(" ")[0])+"'",
+            'return_cmd': "'"+str(cmd_new.split(" ")[1])+"'"
+        }
+
+        return template
+
+command = Execute()
---
+++ cmds/jump.py
@@ -0,0 +1,192 @@
+"""
+Jump
+
+Last Updated: Version 0.0.1
+"""
+
+from re import S
+import subprocess
+from datetime import datetime
+
+from CommandTypes import Command
+import os
+
+class Jump(Command):
+    info = {
+        "title": "Jump",
+        "repository": "https://github.com/SKaplanOfficial/Aria",
+        "documentation_link": "https://github.com/SKaplanOfficial/Aria/documentation/",
+        'id': "aria_jump",
+        "version": "1.0.0",
+        "description": """
+            Jumps to a destination application, file, folder, or website.
+            Tracks jumps to better infer destinations from shorthand notations over time.
+        """,
+        "requirements": {},
+        "extensions": {
+            "aria_exec": "1.0.0",
+            "aria_open": "1.0.0",
+        },
+        "purposes": [
+            "jump to", "go to",
+        ],
+        "targets": [
+            "http://example.com", "google", "documents", "doc", "Applications/Notes.app", "notes",
+        ],
+        "keywords": [
+            "aria", "command", "navigation", "shortcut"
+        ],
+        "example_usage": [
+            ("j downloads", "Opens the downloads folder in the file explorer application (Finder)."),
+            ("j down", "(Likely) Opens the downloads folder."),
+            ("jump google", "Opens google.com."),
+            ("goto goog", "(Likely) Opens google.com."),
+        ],
+        "contact_details": {
+            "author": "Stephen Kaplan",
+            "email": "stephen.kaplan@maine.edu",
+            "website": "http://skaplan.io",
+        }
+    }
+
+    def execute(self, query, origin):
+        # Extract targets from query
+        query_type = self.get_query_type(query)
+        target_destinations = query.split()
+        if query_type != 1:
+            target_destinations = target_destinations[1:]
+        
+        target_destinations = (" ".join(target_destinations)).split(",")
+
+        # Get list of relevant candidates
+        jump_tracker = Command.managers["tracking"].init_tracker("jump")
+        jump_tracker.load_data()
+        candidates = jump_tracker.get_items_containing("targets", target_destinations)
+
+        # Create an object for the target jump(s)
+        now = datetime.now()
+        current_time = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+        target_jump = jump_tracker.new_item([current_time, current_time, 1, target_destinations])
+
+        # Find the best candidate, if one exists
+        best_candidate = self._get_best_candidate(target_jump, candidates, jump_tracker)
+        if best_candidate == None:
+            best_candidate = jump_tracker.new_item([current_time, current_time, 0, target_jump.data["targets"]])
+            jump_tracker.add_item(best_candidate)
+
+        # Attempt to make jump(s)
+        completion_status = self._jump(best_candidate, origin)
+
+        # Report status and update tracker items as necessary
+        if completion_status[0] == 0:
+            # The jump was entirely successful, update the frequency and time of the item
+            if origin in [0, 1]:
+                Command.managers["output"].sprint("Here you go!")
+            self._update_jump(best_candidate, current_time)
+        elif completion_status[0] == -1 and origin in [0, 1]:
+            # Some parts of the jump failed
+            self._report_failures(completion_status[2])
+        else:
+            # The entire jump failed, remove the broken item
+            if origin in [0, 1]:
+                self._report_failures(completion_status[2])
+            self._remove_broken_jump(best_candidate, jump_tracker)
+        
+        jump_tracker.save_data()
+
+    def _get_best_candidate(self, target_jump, candidates, jump_tracker):
+        """ Gets the best candidate jump with matching or near-matching targets, if one exists. Returns None otherwise. """
+        max_freq = jump_tracker.get_max_of_col("frequency")
+
+        def weigh_freq(item_1, item_2):
+            score = jump_tracker.default_compare_method(item_1, item_2)
+
+            for target_1 in item_1.data["targets"]:
+                for target_2 in item_2.data["targets"]:
+                    if target_1.lower() in target_2.lower():
+                        score /= 1.1
+                    else:
+                        score *= 1.01
+
+            score -= (item_2.data["frequency"]) / max_freq
+            return score
+
+        best_candidate = jump_tracker.get_best_match(target_jump, candidates, 0.1, compare_method = weigh_freq)
+        return best_candidate
+
+    def _jump(self, best_candidate, origin):
+        """ Attempts to open the destinations associated with the best candidate. Returns 0 if all jumps succeeded, -1 if some jumps failed while others succeeded, and 1 if all jumps failed. """
+        successes = []
+        failures = []
+
+        for dest in best_candidate.data["targets"]:
+            if origin in [0, 1]:
+                print("Attempting jump to " + dest + "...")
+            command = ["open", dest]
+
+            status = 0
+            if origin != 3:
+                status = subprocess.call(command)
+
+            if status == 0:
+                successes.append(dest)
+            else:
+                failures.append(dest)
+
+        overall_status = 0 # Assume all successes
+        if len(successes) > 0 and len(failures) > 0:
+            # Mix of successes and failures
+            overall_status = -1
+        elif len(successes) == 0:
+            # All failures
+            overall_status = 1
+        
+        return overall_status, successes, failures
+
+    def _update_jump(self, jump_item, current_time):
+        """ Updates the jump item's time range and frequency. """
+        jump_item.data["start_time"] += (current_time - jump_item.data["start_time"]) * 0.01
+        jump_item.data["end_time"] += (current_time - jump_item.data["end_time"]) * 0.01
+        jump_item.data["frequency"] += 1
+
+    def _report_failures(self, failures):
+        """ Reports the destinations that could not be jumped to. """
+        if len(failures) > 1:
+            Command.managers["output"].sprint("I was unable to complete jumps to " + str(len(failures)) + " destinations:")
+            for dest in failures:
+                print("\t" + dest)
+        else:
+            Command.managers["output"].sprint("I was unable to complete the jump to " + failures[0] + ".")
+
+    def _remove_broken_jump(self, jump_item, jump_tracker):
+        """ Removes jump entries that are no longer functional. """
+        if (jump_item.data["frequency"] != 0):
+            print("Found broken jump point, removing.")
+        else:
+            # Item is the temporary item created when no candidate is found
+            Command.managers["output"].dprint("Removing jump item '" + " ".join(jump_item.data["targets"]) + "'")
+        jump_tracker.items.remove(jump_item)
+
+    def get_query_type(self, query):
+        parts = query.split()
+        if parts[0] in ["j", "goto", "jto"]:
+            return 1000
+
+        if "Finder" in Command.managers["context"].current_app:
+            return 1
+        return 0
+
+    def get_template(self, new_cmd_name):
+        print("Enter base command and args: ")
+        cmd_new = input()
+
+        query_length = len(new_cmd_name)+1
+
+        template = {
+            'command': str(cmd_new.split(" ")),
+            'query': 'query['+str(query_length)+':]',
+        }
+
+        return template
+
+command = Jump()
